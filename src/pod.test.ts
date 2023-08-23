7614c8ceec44bc17feecbec6ec3d09afad74c9bc
Frank Schmitt <frank.schmitt@alchemer.com>
2023-08-23 15:58:45 -0700

    Add another test

--- src/pod.test.ts
+++ src/pod.test.ts
@@ -3,8 +3,13 @@ import { processPodspec, processPod } from './pod'
 describe('processPodspec', () => {
   it('parses a podspec', async () => {
     const podspec = await processPodspec('apptentive-fake-plugin.podspec', '.')
-    expect(podspec.name).toEqual(
-      'apptentive-fake-plugin'
-    )
+    expect(podspec.name).toEqual('apptentive-fake-plugin')
+  })
+})
+
+describe('processPod', () => {
+  it("parses a pod's info", async () => {
+    const podspec = await processPod('ApptentiveKit')
+    expect(podspec.name).toEqual('ApptentiveKit')
   })
 })
